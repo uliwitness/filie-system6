dbe879f818047870ffe9c3b1721703913d26abee
Uli Kusterer <ulivc@zathras.de>
2022-07-16 02:26:25 +0200

    Implement a custom LDEF so we can use FSSpecs instead of just names in the file list. Implement odoc/oapp Apple Events because OS 9 doesn't send GetAppFiles anymore it seems.

---
+++ FileLDEF/FileLDEF.h
@@ -0,0 +1,1 @@
+#include <Controls.h>#include <QuickDraw.h>pascal void main(short message, Boolean selected, Rect *cellRect, Cell theCell, short dataOffset,				short dataLen, ListHandle theList);
